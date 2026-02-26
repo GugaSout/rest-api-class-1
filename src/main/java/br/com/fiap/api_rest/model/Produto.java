99496ecae8894c3313463de2454e189d4efe4051
Gustavo-Souto-de-Melo <gsmelo2804@gmial.com>
2026-02-25 21:40:29 -0300

    Segundo commit

--- src/main/java/br/com/fiap/api_rest/model/Produto.java
+++ src/main/java/br/com/fiap/api_rest/model/Produto.java
@@ -1,38 +1,40 @@
 package br.com.fiap.api_rest.model;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Entity
+@Table(name = "TB_PRODUTOS")
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
     private UUID id;
+    @Column(name = "nome_produto")
     private String nome;
+    @Column(name = "preco_produto")
     private BigDecimal preco;
-
-    public Produto(String nome, BigDecimal preco, LocalDate validade) {
-        this.nome = nome;
-        this.preco = preco;
-        this.validade = validade;
-    }
+    @Column(name = "data_validade")
+    private LocalDate expiracao;
 
     public Produto() {
-
     }
 
-    public LocalDate getValidade() {
-        return validade;
-    }
-
-    public void setValidade(LocalDate validade) {
-        this.validade = validade;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
+    public Produto(String nome, BigDecimal preco, LocalDate expiracao) {
+        this.nome = nome;
         this.preco = preco;
+        this.expiracao = expiracao;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
     }
 
     public String getNome() {
@@ -43,15 +45,19 @@ public class Produto {
         this.nome = nome;
     }
 
-
-
-    public UUID getId() {
-        return id;
+    public BigDecimal getPreco() {
+        return preco;
     }
 
-    public void setId(UUID id) {
-        this.id = id;
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
     }
 
-    private LocalDate validade;
+    public LocalDate getExpiracao() {
+        return expiracao;
+    }
+
+    public void setExpiracao(LocalDate expiracao) {
+        this.expiracao = expiracao;
+    }
 }
--- src/main/java/br/com/fiap/api_rest/repository/ProdutoRepository.java
+++ src/main/java/br/com/fiap/api_rest/repository/ProdutoRepository.java
@@ -1,4 +1,9 @@
 package br.com.fiap.api_rest.repository;
 
-public interface ProdutoRepository {
+import br.com.fiap.api_rest.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
 }
